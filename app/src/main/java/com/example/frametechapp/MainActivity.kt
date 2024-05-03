e546b6057642181ceb4344eeaf9a6a3b0991e5d6
ronewa <ronewamaselesele200@gmail.com>
2024-05-03 21:50:13 +0200

    Add a Navigate Drawer for the menu options , to navigate through the pages

--- app/src/main/java/com/example/frametechapp/MainActivity.kt
+++ app/src/main/java/com/example/frametechapp/MainActivity.kt
@@ -3,12 +3,24 @@ package com.example.frametechapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -17,6 +29,14 @@ import com.example.frame_tech_app.Pages.Cart
 import com.example.frame_tech_app.Pages.Homepage
 import com.example.frametechapp.Pages.Profile
 import com.example.frametechapp.ui.theme.FrameTechAppTheme
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.rememberCoroutineScope
+import com.example.frame_tech_app.Pages.About
+import com.example.frame_tech_app.Pages.ProductPage
+import com.example.frame_tech_app.Pages.ServicePage
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,31 +49,64 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeBase(){
     val navController = rememberNavController()
+    val context = LocalContext.current
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-    Scaffold(
-        bottomBar = { TopNavBar(navController = navController)}
-    ) {padding ->
-        Column {
-            NavHost(navController = navController, startDestination = "homepage") {
-                composable("homepage") {
-                    Homepage()
-                }
-                composable("homepage") {
-                    Homepage()
-                }
-                composable("cart") {
-                    Cart()
-                }
-                composable("profile") {
-                    Profile()
-                }
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                NavigateDrawer(navController = navController)
             }
-            Spacer(modifier = Modifier.padding(padding))
+        },
+    ){
+        Scaffold(
+            bottomBar = { TopNavBar(navController = navController)},
+            floatingActionButton = {
+                ExtendedFloatingActionButton(
+                    text = { Text("Menu") },
+                    icon = {  Icon(imageVector = Icons.Filled.Menu, contentDescription = null) },
+                    onClick = {
+                        scope.launch {
+                            drawerState.apply {
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    }
+                )
+            }
+            ) {padding ->
+            Column {
+                NavHost(navController = navController, startDestination = "homepage") {
+                    composable("homepage") {
+                        Homepage()
+                    }
+                    composable("cart") {
+                        Cart()
+                    }
+                    composable("profile") {
+                        Profile()
+                    }
+                    composable("productPage") {
+                        ProductPage()
+                    }
+                    composable("about") {
+                        About()
+                    }
+                    composable("servicePage") {
+                        ServicePage()
+                    }
+                }
+                Spacer(modifier = Modifier.padding(padding))
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/frametechapp/NavigateSection.kt
+++ app/src/main/java/com/example/frametechapp/NavigateSection.kt
@@ -1,11 +1,27 @@
 package com.example.frametechapp
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.HomeRepairService
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Newspaper
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ProductionQuantityLimits
+import androidx.compose.material.icons.filled.Shop
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
@@ -14,6 +30,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -71,4 +92,69 @@ fun TopNavBar(navController: NavController){
             }
         )
     }
+}
+
+@Composable
+fun NavigateDrawer(navController: NavController){
+    Column(
+        modifier = Modifier.fillMaxHeight()
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .clip(shape = RoundedCornerShape(15.dp))
+                .clickable { navController.navigate("homepage")},
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(imageVector = Icons.Filled.Home, contentDescription = null,modifier = Modifier.size(45.dp))
+            Text(text = "Home", color = Color.White, fontSize = MaterialTheme.typography.bodyLarge.fontSize)
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .clip(shape = RoundedCornerShape(15.dp))
+                .clickable { navController.navigate("productPage")},
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(imageVector = Icons.Filled.Shop, contentDescription = null,modifier = Modifier.size(45.dp))
+            Text(text = "Products", color = Color.White, fontSize = MaterialTheme.typography.bodyLarge.fontSize)
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .clip(shape = RoundedCornerShape(15.dp))
+                .clickable { navController.navigate("servicePage")},
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(imageVector = Icons.Filled.HomeRepairService, contentDescription = null,modifier = Modifier.size(45.dp))
+            Text(text = "Service", color = Color.White, fontSize = MaterialTheme.typography.bodyLarge.fontSize)
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .clip(shape = RoundedCornerShape(15.dp))
+                .clickable { navController.navigate("about")},
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(imageVector = Icons.Filled.Newspaper, contentDescription = null,modifier = Modifier.size(45.dp))
+            Text(text = "About Us ", color = Color.White, fontSize = MaterialTheme.typography.bodyLarge.fontSize)
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+    }
+
 }
