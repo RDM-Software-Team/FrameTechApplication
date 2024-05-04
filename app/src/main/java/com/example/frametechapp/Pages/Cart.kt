3a38f6ae7bd2d21b13fba3045b8974f6bb71088e
ronewa <ronewamaselesele200@gmail.com>
2024-05-04 17:04:57 +0200

    Added a CheckOut button in the cart... :)

--- app/src/main/java/com/example/frametechapp/Pages/Cart.kt
+++ app/src/main/java/com/example/frametechapp/Pages/Cart.kt
@@ -14,10 +14,14 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -73,71 +77,83 @@ fun CartTable(cartItems: List<items>, onDelete: (Int) -> Unit){
    ) {
       items(columnNames){ it ->
          Text(text = it, modifier = Modifier
-          .padding(10.dp)
-          .weight(1f, true))
+             .padding(10.dp)
+             .weight(1f, true))
       }
    }
    //The is the Row Section of the table
-    LazyColumn{
-     items(cartItems) { item ->
-         Row(
-          modifier = Modifier
-           .fillMaxWidth()
-           .padding(7.dp)
-           .align(Alignment.CenterHorizontally)
-          ,
-          horizontalArrangement = Arrangement.SpaceBetween,
-          verticalAlignment = Alignment.CenterVertically
-         ) {
-                  Text(
-                       text = item.itemName,modifier = Modifier
-                    .border(1.dp, Color.Black)
-                    .width(80.dp)
-                    .fillMaxHeight())
-                  OutlinedTextField(
-                       value = "${quantity.value}",
-                       onValueChange = { quantity.value = it.toInt() },
-                        modifier = Modifier
-                          .padding(16.dp)
-                          .width(50.dp),
-                       textStyle = TextStyle(fontSize = 12.sp)
-                  )
-                  Text(
-                       text = "R."+item.itemPrice.toString()
-                       , modifier = Modifier
-                       .border(1.dp, Color.Black)
-                       .width(80.dp)
-                       .fillMaxHeight()
-                  )
-                  Text(
-                   text = "R." + (item.itemPrice * quantity.value).toString(),
-                   modifier = Modifier
-                      .border(1.dp, Color.Black)
-                      .width(80.dp)
-                      .fillMaxHeight()
-                  )
-                  IconButton(onClick = {
-                         onDelete(item.itemId)
-                         Toast.makeText(context, "${item.itemId} + ${item.itemName}", Toast.LENGTH_SHORT).show()
-                    },
-                    modifier = Modifier
-                      .padding(5.dp)
-                      .width(80.dp)
-                      .height(30.dp),
-                  ) {
-                      Icon(imageVector = Icons.Default.Delete, contentDescription = null)
-                  }
+    LazyColumn {
+        if (cartItems.isEmpty()) {
+            item {
+                Text(
+                    text = "Cart is Empty",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
             }
-         itemPrices.value += listOf(item.itemPrice)
+        } else {
+            items(cartItems) { item ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(7.dp)
+                        .align(Alignment.CenterHorizontally)
+                    ,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = item.itemName,modifier = Modifier
+                            .border(1.dp, Color.Black)
+                            .width(80.dp)
+                            .fillMaxHeight())
+                    OutlinedTextField(
+                        value = "${quantity.value}",
+                        onValueChange = { quantity.value = it.toInt() },
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .width(50.dp),
+                        textStyle = TextStyle(fontSize = 12.sp)
+                    )
+                    Text(
+                        text = "R."+item.itemPrice.toString()
+                        , modifier = Modifier
+                            .border(1.dp, Color.Black)
+                            .width(80.dp)
+                            .fillMaxHeight()
+                    )
+                    Text(
+                        text = "R." + (item.itemPrice * quantity.value).toString(),
+                        modifier = Modifier
+                            .border(1.dp, Color.Black)
+                            .width(80.dp)
+                            .fillMaxHeight()
+                    )
+                    IconButton(onClick = {
+                        onDelete(item.itemId)
+                        Toast.makeText(context, "${item.itemId} + ${item.itemName}", Toast.LENGTH_SHORT).show()
+                    },
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .width(80.dp)
+                            .height(30.dp),
+                    ) {
+                        Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+                    }
+                }
+                itemPrices.value += (item.itemPrice)
 
-     }
+            }
+
+        }
+        //totalAmount(item = itemPrices.value, quantity = quantity.value)//Display the total amount of all items
+
+       }
+      CheckOut(item = cartItems, totalAmount = totalAmount(item = itemPrices.value, quantity = quantity.value))//Button check out to buy all items
     }
-   Checkout(item = itemPrices.value, quantity = quantity.value)
-
-  }
 }
 @Composable
-fun Checkout(item:List<Double>, quantity:Int){
+fun totalAmount(item:List<Double>, quantity:Int): MutableState<Double>{
     val totalAmount = remember { mutableStateOf(0.0) }
     LaunchedEffect(item, quantity) {
         var total = 0.0
@@ -150,6 +166,65 @@ fun Checkout(item:List<Double>, quantity:Int){
 
     Text(
         text = "Total Amount: R.${totalAmount.value}",
-        modifier = Modifier.padding(10.dp).fillMaxWidth().border(1.dp, Color.Yellow)
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .border(1.dp, Color.Yellow)
     )
+    
+    return totalAmount
+}
+@Composable
+fun CheckOut(item:List<items>, totalAmount:MutableState<Double>){
+    val context = LocalContext.current
+    var openDialog by remember { mutableStateOf(false) }
+
+    OutlinedButton(onClick = {
+        openDialog = true
+        //It must collect all items being brought and the total Amount
+
+        Toast.makeText(context, "Check Out", Toast.LENGTH_SHORT).show()
+    },
+        enabled = if(item.isNotEmpty()) true else false,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp)
+    ) {
+        Text(text = "Check Out")
+    }
+
+    if (openDialog) {
+        AlertDialog(onDismissRequest = {
+            // Dismiss the dialog
+            openDialog = false
+        },
+        title = {Text(text = "Check Out")},
+        confirmButton = {
+            Button(
+                onClick = {
+                    // Dismiss the dialog
+                    openDialog = false
+                }
+            ) {
+                Text(text = "Buy")
+            }
+        },
+            dismissButton = {
+                Button(
+                    onClick = {
+                        // Handle dismiss button click
+                        openDialog = false
+                    }
+                ) {
+                    Text(text = "Cancel")
+                }
+            },
+            containerColor = Color.White,
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+
+
 }
