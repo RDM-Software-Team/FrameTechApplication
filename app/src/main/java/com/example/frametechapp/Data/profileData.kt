949decc929d6dbdc81749df4bd21630aba6e8ccd
ronewa <ronewamaselesele200@gmail.com>
2024-05-04 23:06:32 +0200

    Add a new parameter for the image for the user , Data type is Uri?

--- app/src/main/java/com/example/frametechapp/Data/profileData.kt
+++ app/src/main/java/com/example/frametechapp/Data/profileData.kt
@@ -1,12 +1,16 @@
 package com.example.frame_tech_app.Data
 
-data class profileData(
-    val customerId: String,
-    val firstname: String,
-    val lastname: String,
-    val cellNumber: Number,
-    val email:String,
-    val address:String,
-    val gender: String = "",
-    val cardDetails: List<CardDetails> = emptyList(),//This is the object for holding card details
+import android.net.Uri
+
+data class profileData(//changed val to var this data will be updated by the user if need be
+    var customerId: String,
+    var firstname: String,
+    var lastname: String,
+    var cellNumber: Number,
+    var dob: String,
+    var email:String,
+    var address:String,
+    var gender: String = "",
+    var cardDetails: List<CardDetails> = emptyList(),//This is the object for holding card details
+    var profileImage: Uri? = null,
 )
