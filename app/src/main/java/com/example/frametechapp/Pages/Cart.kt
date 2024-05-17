915a3bf5e9434e9bd7ae58c3b07fc0661f6683fc
ronewa <ronewamaselesele200@gmail.com>
2024-05-17 17:52:05 +0200

    Fixed the problem for the total Amount counting endlessly

--- app/src/main/java/com/example/frametechapp/Pages/Cart.kt
+++ app/src/main/java/com/example/frametechapp/Pages/Cart.kt
@@ -1,5 +1,6 @@
 package com.example.frametechapp.Pages
 
+import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -28,6 +29,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -45,132 +48,150 @@ import com.example.frametechapp.Model.CategoryFilter
 
 @Preview(showBackground = true)
 @Composable
-fun Cart(){
- Column(
-  modifier = Modifier.fillMaxSize(),
-  horizontalAlignment = Alignment.CenterHorizontally,
-  verticalArrangement = Arrangement.Center
- ) {
-  Text(text = "Cart")
-  val listGetters = CategoryFilter()
-  var CartList by remember {mutableStateOf(listGetters.newItemList)}
-  CartTable(cartItems = CartList) { itemId ->//this is the delete function
-   CartList = CartList.filter { it.itemId != itemId }
-  }
- }
-}
-@Composable
-fun CartTable(cartItems: List<items>, onDelete: (Int) -> Unit){
- val context = LocalContext.current
- val columnNames = listOf("Item Name", "Quantity", "Price","SubTotal" ,"Delete")
- var quantity by remember { mutableStateOf<Int>(1) }
- val itemPrices = remember { mutableStateOf<List<Double>>(emptyList()) }
-  Column(
-    modifier = Modifier.fillMaxSize()
-  ) {
-   //This column or the header section of the table
-   LazyRow(
-    modifier = Modifier.fillMaxWidth()
-   ) {
-      items(columnNames){ it ->
-         Text(text = it, modifier = Modifier
-             .padding(10.dp)
-             .weight(1f, true))
-      }
-   }
-   //The is the Row Section of the table
-    LazyColumn {
-        if (cartItems.isEmpty()) {
-            item {
-                Text(
-                    text = "Cart is Empty",
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Center
-                )
-            }
-        } else {
-            items(cartItems) { item ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(7.dp)
-                        .align(Alignment.CenterHorizontally)
-                    ,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = item.itemName,modifier = Modifier
-                            .border(1.dp, Color.Black)
-                            .width(80.dp)
-                            .fillMaxHeight())
-                    OutlinedTextField(
-                        value = quantity.toString(),
-                        onValueChange = {  input ->
-                            try {
-                                val newValue = input.takeIf { it.isNotEmpty() }?.toIntOrNull() ?: 1
-                                quantity += newValue
-                            } catch (e: NumberFormatException) {
-                                // handle invalid input here
-                                quantity = 1 // default to 1 if input is invalid
-                            }
-                        },
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .width(50.dp),
-                        textStyle = TextStyle(fontSize = 12.sp)
-                    )
-                    Text(
-                        text = "R."+item.itemPrice.toString()
-                        , modifier = Modifier
-                            .border(1.dp, Color.Black)
-                            .width(80.dp)
-                            .fillMaxHeight()
-                    )
-                    Text(
-                        text = "R." + (item.itemPrice * quantity).toString(),
-                        modifier = Modifier
-                            .border(1.dp, Color.Black)
-                            .width(80.dp)
-                            .fillMaxHeight()
-                    )
-                    IconButton(onClick = {
-                        onDelete(item.itemId)
-                        Toast.makeText(context, "${item.itemId} + ${item.itemName}", Toast.LENGTH_SHORT).show()
-                    },
-                        modifier = Modifier
-                            .padding(5.dp)
-                            .width(80.dp)
-                            .height(30.dp),
-                    ) {
-                        Icon(imageVector = Icons.Default.Delete, contentDescription = null)
-                    }
-                }
-                itemPrices.value += (item.itemPrice)
-
-            }
-
+fun Cart() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Cart")
+        val listGetters = CategoryFilter()
+        var CartList by remember { mutableStateOf(listGetters.newItemList) }
+        CartTable(cartItems = CartList) { itemId ->
+            // Delete function
+            CartList = CartList.filter { it.itemId != itemId }
         }
-        //totalAmount(item = itemPrices.value, quantity = quantity.value)//Display the total amount of all items
-
-       }
-      CheckOut(item = cartItems, totalAmount = totalAmount(item = itemPrices.value, quantity = quantity))//Button check out to buy all items
     }
 }
-@Composable
-fun totalAmount(item: List<Double>, quantity: Int): MutableState<Double> {
-    val totalAmount = remember { mutableStateOf(0.0) }
 
-    LaunchedEffect(item, quantity) {
-        var total = 0.0
-        for (i in item.indices) {
-            total += item[i]
+@SuppressLint("MutableCollectionMutableState")
+@Composable
+fun CartTable(cartItems: List<items>, onDelete: (Int) -> Unit) {
+    val context = LocalContext.current
+    val columnNames = listOf("Item Name", "Quantity", "Price", "SubTotal", "Delete")
+    val itemQuantities = remember { mutableStateOf(cartItems.map { 1 }.toMutableList()) }
+    val itemPrices = remember { mutableStateOf(cartItems.map { it.itemPrice }.toMutableList()) }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Header section of the table
+        LazyRow(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(columnNames) {
+                Text(
+                    text = it,
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .weight(1f, true)
+                )
+            }
         }
-        totalAmount.value = total * quantity
+
+        // Row section of the table
+        LazyColumn {
+            if (cartItems.isEmpty()) {
+                item {
+                    Text(
+                        text = "Cart is Empty",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            } else {
+                items(cartItems) { item ->
+                    val itemIndex = cartItems.indexOf(item)
+                    var quantity by remember { mutableIntStateOf(1) }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(7.dp)
+                            .align(Alignment.CenterHorizontally),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = item.itemName,
+                            modifier = Modifier
+                                .border(1.dp, Color.Black)
+                                .width(80.dp)
+                                .fillMaxHeight()
+                        )
+                        OutlinedTextField(
+                            value = quantity.toString(),
+                            onValueChange = { input ->
+                                try {
+                                    val newValue = input.toIntOrNull() ?: 1
+                                    if (newValue > 0) {
+                                        quantity = newValue
+                                        itemQuantities.value[itemIndex] = newValue
+                                    }
+                                } catch (e: NumberFormatException) {
+                                    // Handle invalid input here
+                                    quantity = 1 // Default to 1 if input is invalid
+                                }
+                            },
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .width(50.dp),
+                            textStyle = TextStyle(fontSize = 12.sp)
+                        )
+                        Text(
+                            text = "R." + item.itemPrice.toString(),
+                            modifier = Modifier
+                                .border(1.dp, Color.Black)
+                                .width(80.dp)
+                                .fillMaxHeight()
+                        )
+                        Text(
+                            text = "R." + (item.itemPrice * quantity).toString(),
+                            modifier = Modifier
+                                .border(1.dp, Color.Black)
+                                .width(80.dp)
+                                .fillMaxHeight()
+                        )
+                        IconButton(
+                            onClick = {
+                                onDelete(item.itemId)
+                                Toast.makeText(
+                                    context,
+                                    "${item.itemId} + ${item.itemName}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            },
+                            modifier = Modifier
+                                .padding(5.dp)
+                                .width(80.dp)
+                                .height(30.dp),
+                        ) {
+                            Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+                        }
+                    }
+                }
+            }
+        }
+
+        val totalAmount = totalAmount(itemPrices.value, itemQuantities.value)
+        CheckOut(cartItems, totalAmount)
+    }
+}
+
+@Composable
+fun totalAmount(prices: List<Double>, quantities: List<Int>): MutableState<Double> {
+    val totalAmount = remember { mutableDoubleStateOf(0.0) }
+
+    LaunchedEffect(prices, quantities) {
+        var total = 0.0
+        for (i in prices.indices) {
+            total += prices[i] * quantities[i]
+        }
+        totalAmount.doubleValue = total
     }
 
     Text(
-        text = "Total Amount: R.${totalAmount.value}",
+        text = "Total Amount: R.${totalAmount.doubleValue}",
         modifier = Modifier
             .padding(10.dp)
             .fillMaxWidth()
@@ -179,18 +200,20 @@ fun totalAmount(item: List<Double>, quantity: Int): MutableState<Double> {
 
     return totalAmount
 }
+
 @Composable
-fun CheckOut(item:List<items>, totalAmount:MutableState<Double>){
+fun CheckOut(cartItems: List<items>, totalAmount: MutableState<Double>) {
     val context = LocalContext.current
     var openDialog by remember { mutableStateOf(false) }
 
-    OutlinedButton(onClick = {
-        openDialog = true
-        //It must collect all items being brought and the total Amount
+    OutlinedButton(
+        onClick = {
+            openDialog = true
+            // It must collect all items being bought and the total amount
 
-        Toast.makeText(context, "Check Out", Toast.LENGTH_SHORT).show()
-    },
-        enabled = if(item.isNotEmpty()) true else false,
+            Toast.makeText(context, "Check Out", Toast.LENGTH_SHORT).show()
+        },
+        enabled = cartItems.isNotEmpty(),
         modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth(),
@@ -200,21 +223,22 @@ fun CheckOut(item:List<items>, totalAmount:MutableState<Double>){
     }
 
     if (openDialog) {
-        AlertDialog(onDismissRequest = {
-            // Dismiss the dialog
-            openDialog = false
-        },
-        title = {Text(text = "Check Out")},
-        confirmButton = {
-            Button(
-                onClick = {
-                    // Dismiss the dialog
-                    openDialog = false
+        AlertDialog(
+            onDismissRequest = {
+                // Dismiss the dialog
+                openDialog = false
+            },
+            title = { Text(text = "Check Out") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        // Dismiss the dialog
+                        openDialog = false
+                    }
+                ) {
+                    Text(text = "Buy")
                 }
-            ) {
-                Text(text = "Buy")
-            }
-        },
+            },
             dismissButton = {
                 Button(
                     onClick = {
@@ -230,6 +254,4 @@ fun CheckOut(item:List<items>, totalAmount:MutableState<Double>){
             modifier = Modifier.padding(16.dp)
         )
     }
-
-
 }
