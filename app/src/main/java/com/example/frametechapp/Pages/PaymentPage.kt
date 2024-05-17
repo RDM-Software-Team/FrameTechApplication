0c9d35589ac10db52af05d28306fe4cbb8a49cc6
ronewa <ronewamaselesele200@gmail.com>
2024-05-17 17:53:22 +0200

    This page is not functioning

--- app/src/main/java/com/example/frametechapp/Pages/PaymentPage.kt
+++ app/src/main/java/com/example/frametechapp/Pages/PaymentPage.kt
@@ -1,8 +1,9 @@
 package com.example.frame_tech_app.Pages
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun Payment(){
-
+    Text(text = "Payment Mode coming soon")
 }
