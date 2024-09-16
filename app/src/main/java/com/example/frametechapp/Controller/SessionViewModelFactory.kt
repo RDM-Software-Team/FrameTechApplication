e56895b87e689e7032da1e96df5b9916475d05fd
ronewa <ronewamaselesele200@gmail.com>
2024-09-16 19:10:57 +0200

    Made a change to read the NetworkManager class

--- app/src/main/java/com/example/frametechapp/Controller/SessionViewModelFactory.kt
+++ app/src/main/java/com/example/frametechapp/Controller/SessionViewModelFactory.kt
@@ -1,13 +1,17 @@
 package com.example.frametechapp.Controller
 
+import android.app.Application
+import android.content.Context
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class SessionViewModelFactory(private val repository: SessionManager ) : ViewModelProvider.Factory {
+class SessionViewModelFactory(private val repository: SessionManager,private val context: Context
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(SessionViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return SessionViewModel(sessionManager = repository, NetworkManager()) as T
+            return SessionViewModel(sessionManager = repository, NetworkManager(), context =context) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
